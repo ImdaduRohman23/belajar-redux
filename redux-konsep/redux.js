c53f21306257f8a5211b5a3904adc4fc7f24243c
ImdaduRohman23 <ir.imdadurohman@gmail.com>
2022-08-11 23:05:29 +0700

    membuat file

---
+++ redux-konsep/redux.js
@@ -0,0 +1,18 @@
+// const redux = require('redux');
+// const createStore = redux.createStore;
+
+
+//reducer => berisi kumpulan fungsi yang akan memanipulasi atau mengubah state pada store
+// const rootReducer = (state, action) => {
+//     return state;
+// }
+
+//store => wadah penyimpanan state scr global
+// const store = createStore(rootReducer);
+// console.log(store.getStore())
+
+
+//dispatch/action => proses pemanggilan fungsi yang tersedia pada reducer
+
+//subscription => proses penggunaan store yang dimiliki
+
